ea9cea1ee6224fa64e2c87c47f12e9b6a6e6ca8f
Andreas Neumann <a.neumann@carto.net>
2019-06-24 15:32:10 +0200

    adding initial Dockerfile, build-scripts and conf

--- docker_rebuild.sh
+++ docker_rebuild.sh
@@ -2,9 +2,9 @@
 docker build -t 'sogis/qgis-server-v3:master' .
 # set +e means "exit immediately if a command exits with a non-zero result status"
 set +e
-docker rm -f qgis-server
+docker rm -f qgis-server-v3
 set -e
-docker run -p 9991:9991 --name qgis-server -d --restart=always sogis/qgis-server-v3:master
+docker run -p 9991:9991 --name qgis-server-v3 -d --restart=always sogis/qgis-server-v3:master
 echo
 echo "#####################################################"
 echo "Use the following comand to see QGIS server live logs"
